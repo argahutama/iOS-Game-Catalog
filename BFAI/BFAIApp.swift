a3c552475e4da43612acdd7f2a1ee49f7549b1fa
argahutama <argahut@gmail.com>
2022-09-11 16:11:20 +0700

    Init

---
+++ BFAI/BFAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFAIApp.swift
+//  BFAI
+//
+//  Created by Arga Hutama on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BFAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BFAI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BFAI
+//
+//  Created by Arga Hutama on 11/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
